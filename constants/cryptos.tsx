900631bd22f263407a55d235d413e9f5c79de2ab
Suraj Dutta <surajdutta@Surajs-MacBook-Pro.local>
2022-06-12 07:43:35 +0800

    feat: added constants for cryptos and currencySymbols

---
+++ constants/cryptos.tsx
@@ -0,0 +1,10 @@
+export const cryptos: any = {
+    "btc": "Bitcoin",
+    "eth": "Ether",
+    "ltc": "Litecoin",
+    "xmr": "Monero",
+    "xrp": "Ripple",
+    "doge": "Dogecoin",
+    "dash": "Dash",
+    "lsk": "Lisk"
+}
---
+++ constants/currencySymbols.tsx
@@ -0,0 +1,5 @@
+export const currencySymbols: any = {
+    usd: "$",
+    gbp: "£",
+    eur: "€",
+}
