754ed80971c48b4b16d272bebd9b4b6bf257b776
Suraj Dutta <surajdutta@Surajs-MacBook-Pro.local>
2022-06-12 08:50:35 +0800

    feat: added Cryptocurrencies page

---
+++ pages/cryptocurrencies/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Cryptocurrencies.module.css'
+import Crypto from '../../components/Cryptocurrencies/Crypto'
+import { CryptoType } from '../../types/cryptos'
+
+const Cryptocurrencies: NextPage<{cryptos: CryptoType[]}> = ({cryptos}) => {
+
+  const [cryptocurrencies, setCryptocurrencies] = useState<CryptoType[]>(cryptos);
+  
+  return (
+    <div>
+      {cryptocurrencies.map((item, index) => <Crypto key={index} data={item} />)}
+    </div>
+  )
+}
+
+export default Cryptocurrencies
+
+
+export async function getServerSideProps(context: any) {
+
+  const response = await fetch('http://localhost:3001/cryptos')
+  const data = await response.json()
+
+  return {
+    props: {
+      cryptos: data,
+    },
+  }
+}
